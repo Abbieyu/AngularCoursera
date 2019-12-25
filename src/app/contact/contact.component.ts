64de38a22b6117f4d47f71113b0f0a577cffca80
AbieYu <aub.ataa@gmail.com>
2019-12-25 15:35:56 +0200

    Angular reactive forms part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,23 +10,55 @@ export class ContactComponent implements OnInit {
   feedbackForm:FormGroup;
   feedback:Feedback;//the corresponding data model.
   contactType = ContactType;
-  @ViewChild('feedbackform') feedbackFormDirective;//reference to the feedbackform
-
+  //gives access to the DOM element with the same name in the brackets
+  @ViewChild('FeedbackForm') feedbackFormDirective;//reference to the feedbackform
+  formErrors = {//an object containing the errors
+    'firstname':'',
+    'lastname':'',
+    'telnum':'',
+    'email':''
+  };
+  validationMessages={// an object containing validation(error) messages
+    'firstname':{
+      'required':'First name is required.',
+      'minlength':'First name must be at least 2 characters long.',
+      'maxlength':'First name cannot exceed 25 characters.'
+    },
+    'lastname':{
+      'required':'Last name is required.',
+      'minlength':'Last name must be at least 2 characters long.',
+      'maxlength':'Last name cannot exceed 25 characters.'
+    },
+    'telnum':{
+      'required':'Tel. no. is required.',
+      'pattern':'Tel. no. must only contain numbers.'
+    },
+    'email':{
+      'required':'Email is required.',
+      'email':'Email is invalid.'
+    }
+  }
   constructor(private formBuilder:FormBuilder) {
     this.createForm();
    }
 
   createForm(){
     this.feedbackForm=this.formBuilder.group({
-      firstname:['',Validators.required],
-      lastname:['',Validators.required],
-      telnum:[0,Validators.required],
-      email:['',Validators.required],
+      firstname:['',[Validators.required, Validators.minLength(2),Validators.maxLength(25)]],
+      lastname:['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
+      telnum:[0,[Validators.required,Validators.pattern]],
+      email:['',[Validators.required,Validators.email]],
       agree:false,
       contacttype:'None',
       message:''
     });
+    //angular provides the valueChanges observable when there are changes in the form
+    this.feedbackForm.valueChanges
+      .subscribe(data=>this.onValueChanged(data));//initiates form validation and defines error messages
+
+      this.onValueChanged();// reset form validation messages
   }
+
   onSubmit(){
     console.log('here!');
     this.feedback = this.feedbackForm.value;
@@ -41,6 +73,25 @@ export class ContactComponent implements OnInit {
       message:''
     });
     this.feedbackFormDirective.resetForm();//reset the form using @viewChild
+  }
+  onValueChanged(data?:any)
+  {
+    if(!this.feedbackForm)
+      {return;}
+    const form = this.feedbackForm;
+    for(const field in this.formErrors){
+      if(this.formErrors.hasOwnProperty(field)){//clear previous error messages (if any)
+        this.formErrors[field]='';
+        const control=form.get(field);//the value in the field?
+        if(control && control.dirty && !control.valid){
+            const messages = this.validationMessages[field];
+            for (const key in control.errors){
+              if(control.errors.hasOwnProperty(key))
+                this.formErrors[field] +=messages[key] + ' ';
+            }
+        }
+      }
+    }
   }
   ngOnInit() {
   }
